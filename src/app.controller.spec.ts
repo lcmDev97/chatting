a30749d6345af9475d7fd173581efbdc336ee7b0
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-11-28 17:10:21 +0900

    [update] first commit

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('basic test', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+
+    it('should be 4', () => {
+      expect(2+2).toEqual(4)
+    })
+
+  });
+
+});
