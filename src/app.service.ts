f63c40728805d98fd7cc0146b1cd5dd91902d019
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-11-29 17:05:59 +0900

    [fix] testing to github actions

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '해냈다!!';
+    return '깃헙 액션 CI 잘 작동함. 이제 test code 적용하자';
   }
 }
