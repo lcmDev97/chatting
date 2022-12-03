7fc68f7d4d807082bd5ea77acfa34d3d1c82226f
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-12-03 16:43:21 +0900

    [update] scripts 폴더 내의 코드 수정

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
+cd /home/ubuntu/chattingApp
 
-DOCKER_IMAGE_NAME=changmin97/chatting-image
-
-DOCKER_CONTAINER_NAME=chattingApp
-
-docker build -t ${DOCKER_IMAGE_NAME} ../ # <--  Dockerfile 상대 경로 지정.
-
-docker run -d -p 80:80 --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
+sh scripts/docker-script.sh
