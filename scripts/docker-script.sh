63b98af786827b270ce086ecd747c269f99c7737
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-12-03 17:00:15 +0900

    [fix] sh 문법 테스트중

--- scripts/docker-script.sh
+++ scripts/docker-script.sh
@@ -2,6 +2,6 @@ DOCKER_IMAGE_NAME=changmin97/chatting-image
 
 DOCKER_CONTAINER_NAME=chattingApp
 
-docker build -t ${DOCKER_IMAGE_NAME} ../ # <--  Dockerfile 상대 경로 지정.
+docker build -t changmin97/chatting-image ../ # <--  Dockerfile 상대 경로 지정.
 
-docker run -d -p 80:80 --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
+docker run -d -p 80:80 --name chattingApp changmin97/chatting-image
