88a0b3665c82453c4cb86b1d8b10efbcd32a0c2f
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-12-03 16:07:37 +0900

    [update] execute.sh 추가, appspec.yml에 hook 추가

---
+++ scripts/execute.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOCKER_IMAGE_NAME=changmin97/chatting-image
+
+DOCKER_CONTAINER_NAME=chattingApp
+
+docker build -t ${DOCKER_IMAGE_NAME} ../ # <--  Dockerfile 상대 경로 지정.
+
+docker run -d -p 80:80 --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
