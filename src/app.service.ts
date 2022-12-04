1f3e3d76e03d7edd9af83c64f3d11cc7e644c10e
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-12-04 16:43:41 +0900

    [fix] codedeploy 테스트 중

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'i hate codedeploy';
+    return 'i love codedeploy';
   }
 }
