c120ebcde2aba4283f9403f77bf7192173e1e8a6
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-12-04 16:37:30 +0900

    [fix] codedeploy 테스트 중

--- scripts/docker-script.bash
+++ scripts/docker-script.bash
@@ -2,4 +2,4 @@ cd /home/ubuntu
 
 sudo docker pull changmin97/chatting-image:latest
 
-sudo docker run -d -p 3001:80 --name chatting-container changmin97/chatting-image:latest
+sudo docker run -d --rm -p 3001:80 --name chatting-container changmin97/chatting-image:latest
