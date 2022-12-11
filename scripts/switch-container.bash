26866ae972e8c5a9ebddc7805c4b62cda217cf02
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-12-11 17:48:33 +0900

    [fix] 무중단 배포 테스트중

--- scripts/switch-container.bash
+++ scripts/switch-container.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "cd후 pwd 진행합니다."
+cd /home/ubuntu/chattingApp
+pwd
+ls
 
 DOCKER_APP_NAME=nestapp
 EXIST_GREEN=$(docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml ps | grep running)
