148664c67ce043d7b402adb52e87306739bc4b01
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-12-12 16:05:00 +0900

    [update] mongodb, env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+import { NestModule } from '@nestjs/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import mongoose from 'mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === "dev" ? true : false;
+    mongoose.set('debug', DEBUG)
+  }
+}
