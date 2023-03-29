23ad0b19b73a90f95acb228c57c27861df94d877
lcmDev97 <lcmdev97@gmail.com>
2023-03-29 16:17:14 +0900

    [update] redis-adapter 설정

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { RedisIoAdapter } from './redis.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
   app.useGlobalPipes(new ValidationPipe())
+  app.useWebSocketAdapter(redisIoAdapter);
 
   await app.listen(process.env.PORT);
 }
